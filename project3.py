4658164477db1e4517f253b086e8681189a9f40e
alexm-torres <alexm.torres@tamu.edu>
2021-09-27 18:33:35 -0500

    Update project3.py

--- project3.py
+++ project3.py
@@ -1,38 +1,28 @@
-import http_access_log.txt
-import requests
-
 with open("http_access_log.txt") as file_in:
     array = []
     for line in file_in:
         array.append(line)
 
-for i in array:
-    print(array[i])
-#    if array[i].find("Apr") != -1 and array[i].find("1995") != -1:
- #       array[i].append("4")
-  #  elif array[i].find("May") != -1 and array[i].find("1995") != -1:
-   #     array[i].append("5")
-    #elif array[i].find("Jun") != -1 and array[i].find("1995") != -1:
-     #   array[i].append("6")
-    #elif array[i].find("Jul") != -1 and array[i].find("1995") != -1:
-     #   array[i].append("7")
-    #elif array[i].find("Aug") != -1 and array[i].find("1995") != -1:
-     #   array[i].append("8")
-    #elif array[i].find("Sep") != -1 and array[i].find("1995") != -1:
-     #   array[i].append("9")
-    #elif array[i].find("Oct") != -1 and array[i].find("1995") != -1:
-     #   array[i].append("10")
-    #else:
-     #   array[i] = array[i]
+count = 0
 
-countsix = 0
+for i in array:
+    if i.find("Apr") != -1 and i.find("1995") != -1:
+        count += 1
+    elif i.find("May") != -1 and i.find("1995") != -1:
+        count += 1
+    elif i.find("Jun") != -1 and i.find("1995") != -1:
+        count += 1
+    elif i.find("Jul") != -1 and i.find("1995") != -1:
+        count += 1
+    elif i.find("Aug") != -1 and i.find("1995") != -1:
+        count += 1
+    elif i.find("Sep") != -1 and i.find("1995") != -1:
+        count += 1
+        array[count] = i
+    elif i.find("Oct") != -1 and i.find("1995") != -1:
+        count += 1
+
 countall = len(array)
 
-for i in array:
-    if array[i].endswith("4"):
-        countsix = len(array) - countall
-        break
-
-
-print("Total Number of Requests in the past six months: ", countsix)
-print("Total Number of Requests: ", countall) 
+print("Total Number of Requests in the past six months: ", count)
+print("Total Number of Requests: ", countall)\
